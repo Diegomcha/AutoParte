8a80cce32a6431d3a8382906ba33af976cf43d19
Diego <38155922+Diegomcha@users.noreply.github.com>
2026-07-05 19:33:54 +0200

    ci(frontend): :bug: fix frontend build

--- frontend/app/routes/admin/index.tsx
+++ frontend/app/routes/admin/index.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'react-router';
 
-export function loader() {
+export function clientLoader() {
 	// Redirect to the employees page by default when accessing the admin route.
 	return redirect('/admin/employees');
 }
